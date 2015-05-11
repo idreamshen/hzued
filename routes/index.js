4666505cc1a8caf055ea16c22f1c8ca9aecda881
沈星宇 <idream.shen@gmail.com>
2015-05-11 08:55:58 +0800

    增加多种路由处理

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,19 @@ var router = require('koa-router')();
 var site = require('../controllers/site.js');
 var user = require('../controllers/user.js');
 var topic = require('../controllers/topic.js');
+var admin = require('../controllers/admin.js');
+var reply = require('../controllers/reply.js');
 
 
 router.get('/', site.index);
-router.get('/topic/:id', topic.index);
-router.post('/admin/login', user.adminLogin);
-router.get('/admin/login', user.adminLogin);
-router.get('/admin', user.adminDashboard);
+router.get('/topic/:topic_id', topic.index);
+router.post('/topic/:topic_id/reply', reply.add);
+
+router.get('/admin', admin.index);
+router.post('/admin/login', admin.login);
+router.get('/admin/login', admin.login);
+router.post('/admin/topic/add', topic.add);
+router.post('/admin/topic/:topic_id/del', topic.del);
+router.post('/admin/topic/:topic_id/edit', topic.edit);
 
 module.exports = router;
