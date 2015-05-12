af7d7a9db70abf00025e89c8b6439ad8a8d8f437
沈星宇 <idream.shen@gmail.com>
2015-05-13 00:31:47 +0800

    增加首页数据读取

--- controllers/site.js
+++ controllers/site.js
@@ -1,8 +1,26 @@
+var Topic = require('../proxy/').Topic;
+var User = require('../proxy/').User;
+
 /**
  * @desc 首页控制器
  * @param next
  */
 exports.index = function *(next) {
-  this.render("index");
+  var topics = yield Topic
+    .find({},{},{
+      limit:10,
+      sort: {
+        create_at:-1
+      }
+    });
+  for (var i in topics) {
+    var topic = topics[i];
+    var authorid = topic.author_id;
+    var user = yield User.getUserById(authorid);
+    var nickname = user.nickname;
+    topic.author_nickname = nickname;
+  }
+  this.render("index", {
+    topics: topics
+  });
 };
-
