639f0111e1d614dd25ccd3b3674a7fe923f099a5
沈星宇 <idream.shen@gmail.com>
2015-05-20 23:56:59 +0800

    添加登陆注册路由

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,10 @@
 var User = require('../proxy').User;
 var Topic = require('../proxy').Topic;
 
+exports.login = function *(next) {
+
+};
+
+exports.register = function *(next) {
+
+};
--- routes/index.js
+++ routes/index.js
@@ -10,19 +10,36 @@ var admin = require('../controllers/admin.js');
 var reply = require('../controllers/reply.js');
 var ftk = require('../controllers/ftk.js');
 
-
+/**
+ * 首页
+ */
 router.get('/', site.index);
+/**
+ * 话题
+ */
 router.get('/topic/:topic_id', topic.index);
 router.post('/topic/:topic_id/reply', reply.add);
-
+/**
+ * 登陆
+ */
+router.get('/login', user.login);
+router.post('/login', user.login);
+/**
+ * 注册
+ */
+router.get('/register', user.register);
+router.post('/register', user.register);
+/**
+ * 天黑请闭眼，find the killer
+ */
 router.get('/ftk/', ftk.index);
-
+/**
+ * 后台
+ */
 router.get('/admin', admin.index);
-
 router.post('/admin/login', admin.login);
 router.get('/admin/login', admin.login);
 router.post('/admin/logout', admin.logout);
-
 router.post('/admin/topic/add', topic.add);
 router.post('/admin/topic/:topic_id/del', topic.del);
 router.post('/admin/topic/:topic_id/edit', topic.edit);
