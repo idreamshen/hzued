914a452c1d3c1f57335b1e9b804079c6f1605ca0
沈星宇 <idream.shen@gmail.com>
2015-05-21 00:12:46 +0800

    模板渲染

--- controllers/user.js
+++ controllers/user.js
@@ -2,9 +2,9 @@ var User = require('../proxy').User;
 var Topic = require('../proxy').Topic;
 
 exports.login = function *(next) {
-
+  this.render('login');
 };
 
 exports.register = function *(next) {
-
+  this.render('register');
 };
