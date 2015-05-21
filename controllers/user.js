30ce05a09667ca49e19059241a408c3b371b7fff
沈星宇 <idream.shen@gmail.com>
2015-05-21 20:59:01 +0800

    完善登录

--- controllers/user.js
+++ controllers/user.js
@@ -2,9 +2,29 @@ var User = require('../proxy').User;
 var Topic = require('../proxy').Topic;
 
 exports.login = function *(next) {
-  this.render('login');
+  if (this.method === 'GET') {
+    this.render('login');
+  } else if (this.method === 'POST') {
+    // TODO
+    var username = this.post.username;
+    var password = this.post.password;
+    var user = yield User.getUser(username, password);
+    if (user) {
+      this.session.user = {
+        _id: user._id,
+        nickname: user.nickname
+      };
+      this.redirect('/');
+    } else {
+      this.body = '账号或密码错误';
+    }
+  }
 };
 
 exports.register = function *(next) {
-  this.render('register');
+  if (this.method === 'GET') {
+    this.render('register');
+  } else if (this.method === 'POST') {
+    // TODO
+  }
 };
